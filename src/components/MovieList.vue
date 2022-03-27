fe31979177f4ca5b89ba619983932ee6b79546b5
shinbian11 <shinbian11@gmail.com>
2022-03-27 09:20:07 +0900

    17. 영화 검색 코드 리팩토링 >> 1) 영화 검색 메서드를 비동기 메서드로 변경 >> 2) _fetchMovie() 라는 메서드로 분리하여 기능이 확장되어 복잡해졌을때의 재활용성을 높임

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
     <div class="inner">
+      <!-- 오류 메세지 출력 -->
+      <div class="message">
+        {{ message }}
+      </div>
       <!-- v-for="movie in movies" 에서 movies는 computed 의 movies (배열 데이터) 이다. -->
       <MovieItem v-for="movie in movies" :key="movie.imdbID" :movie="movie" />
     </div>
@@ -15,7 +19,10 @@ export default {
   computed: {
     // 반응성이 유지된 데이터를 가져와야 하므로, computed에 정의한 것!
     movies() {
-      return this.$store.state.movie.movies;
+      return this.$store.state.movie.movies; // store의 상태(state) 부분의 movie 라는 모듈 (movie.js) 안에 있는 movies
+    },
+    message() {
+      return this.$store.state.movie.message; // store의 상태(state) 부분의 movie 라는 모듈 (movie.js) 안에 있는 message
     },
   },
 };
--- src/store/movie.js
+++ src/store/movie.js
@@ -26,40 +26,70 @@ export default {
     },
   },
   actions: {
+    // payload : 받아오는 데이터
+    // Search movies 기능
     async searchMovies({ state, commit }, payload) {
-      // payload : 받아오는 데이터
-      const { title, type, number, year } = payload;
       // async, await : 비동기 처리
-      // Search movies 기능
-      const OMDB_API_KEY = "7035c60c"; // 제공받은 키, 개인적으로 하려면 http://www.omdbapi.com/ 홈페이지의 API Key 에서 발급
-      const res = await axios.get(
-        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
-      );
+      try {
+        const res = await _fetchMovie({
+          ...payload,
+          page: 1,
+        });
 
-      const { Search, totalResults } = res.data;
-      commit("updateState", {
-        movies: _uniqBy(Search, "imdbID"), // imdbID를 기준으로 중복을 제거한 데이터만 movies에 저장된다. (imdbID가 동일한 데이터들이 중복되어 검색되는 경우가 있으므로!)
-      }); // 두 번째 객체 데이터가 updateState 메서드의 payload로 들어간다.
+        const { Search, totalResults } = res.data;
+        commit("updateState", {
+          movies: _uniqBy(Search, "imdbID"), // imdbID를 기준으로 중복을 제거한 데이터만 movies에 저장된다. (imdbID가 동일한 데이터들이 중복되어 검색되는 경우가 있으므로!)
+        }); // commit() 의 두 번째 인자인 객체 데이터가 updateState 메서드의 payload로 들어간다.
 
-      const total = parseInt(totalResults, 10);
-      const pageLength = Math.ceil(total / 10); // 총 268개의 데이터가 검색되었다면, 1 page에 10개라면 총 27 page 이다
+        const total = parseInt(totalResults, 10);
+        const pageLength = Math.ceil(total / 10); // 총 268개의 데이터가 검색되었다면, 1 page에 10개라면 총 27 page 이다
 
-      // 추가 요청!
-      if (pageLength > 1) {
-        for (let page = 2; page <= pageLength; page += 1) {
-          if (page > number / 10) break;
-          const res = await axios.get(
-            `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-          );
-          const { Search } = res.data;
-          commit("updateState", {
-            movies: [
-              ...state.movies, // 전개 연산자 사용
-              ..._uniqBy(Search, "imdbID"), // imdbID를 기준으로 중복을 제거한 데이터만 movies에 저장된다. (imdbID가 동일한 데이터들이 중복되어 검색되는 경우가 있으므로!)
-            ], // 기존의 movies 데이터에 들어가 있는 데이터(state.movies)에다가 이번에 추가된 데이터(_uniqBy(Search, "imdbID"))를 삽입
-          });
+        // 영화 데이터 추가 요청!
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1) {
+            if (page > payload.number / 10) break;
+            const res = await _fetchMovie({
+              ...payload,
+              page, // page: page,
+            });
+            const { Search } = res.data;
+            commit("updateState", {
+              movies: [
+                ...state.movies, // 전개 연산자 사용
+                ..._uniqBy(Search, "imdbID"), // imdbID를 기준으로 중복을 제거한 데이터만 movies에 저장된다. (imdbID가 동일한 데이터들이 중복되어 검색되는 경우가 있으므로!)
+              ], // 기존의 movies 데이터에 들어가 있는 데이터(state.movies)에다가 이번에 추가된 데이터(_uniqBy(Search, "imdbID"))를 삽입
+            });
+          }
         }
+      } catch (message) {
+        // 데이터 요청하여 가져오던 와중에 문제가 발생했을 때
+        commit("updateState", {
+          // movies 는 빈 배열로 초기화, 오류 메세지만 보내서 출력한다.
+          movies: [],
+          message, // message: message,
+        });
       }
     },
   },
 };
+
+// 영화 검색 메소드
+function _fetchMovie(payload) {
+  const { title, type, year, page } = payload;
+  const OMDB_API_KEY = "7035c60c"; // 제공받은 키, 개인적으로 하려면 http://www.omdbapi.com/ 홈페이지의 API Key 에서 발급
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((res) => {
+        if (res.data.Error) {
+          reject(res.data.Error); // searchMovies 비동기 메서드의 catch 문으로 간다.
+        }
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err.message); // searchMovies 비동기 메서드의 catch 문으로 간다.
+      });
+  });
+}
