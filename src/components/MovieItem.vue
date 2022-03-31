1566a79e2519405bb63b3a7d75a1805f2d2e49e5
shinbian11 <shinbian11@gmail.com>
2022-04-01 07:48:13 +0900

    19. 영화 아이템 - 텍스트 말줄임 표시와 배경 흐림 처리 >> backdrop-filter의 blur 함수와 css 조합을 이용한 텍스트 말줄임 표시

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -38,6 +38,16 @@ export default {
   background-size: cover; // background image인 포스터의 비율을 더 넓은 길이인 세로에 맞춘다.
   overflow: hidden;
 
+  &:hover::after {
+    // 영화 포스터 hover시 노란색 테두리 보이도록 (노란색 테두리를 가상 요소 after를 이용하여 표현함)
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    border: 6px solid $primary; // $primary 는 노란색 계열의 색상
+  }
   .info {
     width: 100%;
     padding: 14px;
@@ -48,6 +58,20 @@ export default {
     left: 0;
     bottom: 0;
     background-color: rgba($black, 0.3);
+
+    backdrop-filter: blur(10px); // 뒷배경 blur 처리
+
+    .year {
+      color: $primary;
+    }
+    .title {
+      color: $white;
+
+      // text가 영역을 넘어가면 ... 처리
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
   }
 }
 </style>
